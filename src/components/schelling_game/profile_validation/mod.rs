e628590880e239baf1eab0301e6c7fce11979d24
Amiya Behera <amiyatulu@gmail.com>
2024-02-21 16:55:25 +0530

    change period

--- src/components/schelling_game/profile_validation/mod.rs
+++ src/components/schelling_game/profile_validation/mod.rs
@@ -1,6 +1,11 @@
 pub mod add_profile;
 pub mod add_profile_sign_in;
+pub mod add_profile_stake;
+pub mod add_profile_stake_sign_in;
 pub mod apply_jurors;
 pub mod apply_jurors_sign_in;
+pub mod challenge_evidence;
+pub mod challenge_evidence_sign_in;
+pub mod change_period;
 pub mod fetch_ipfs_profile;
 pub mod view_profile_from_address;
